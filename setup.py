21711b4bd73f93108a56b7fae7688d2c4e8838e1
Kenta Sato <bicycle1885@gmail.com>
2014-03-22 16:01:49 +0900

    drop Cython dependency

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 import os
 from sys import exit
 from setuptools import setup, Extension
-from Cython.Build import cythonize
 import numpy as np
+
+try:
+    from Cython.Build import cythonize
+    have_cython = True
+except ImportError:
+    have_cython = False
 
 try:
     julia_home = os.environ["JULIA_HOME"]
@@ -29,19 +34,29 @@ def numpy_include_dirs():
     return [np.get_include()]
 
 
-core = Extension("core",
-                 [join("libjulia", "core.pyx")],
-                 include_dirs=(julia_include_dirs(julia_home)
-                               + numpy_include_dirs()),
-                 libraries=["julia"],
-                 runtime_library_dirs=[join(julia_home, "..", "lib")],
-                 define_macros=[])
+def generate_extension(name, sources):
+    return Extension(
+        name,
+        sources,
+        include_dirs=(julia_include_dirs(julia_home)
+                      + numpy_include_dirs()),
+        libraries=["julia"],
+        runtime_library_dirs=[join(julia_home, "..", "lib")],
+        define_macros=[]
+    )
+
+
+if have_cython:
+    core = generate_extension("core", [join("libjulia", "core.pyx")])
+    ext_modules = cythonize([core])
+else:
+    core = generate_extension("core", [join("libjulia", "core.c")])
+    ext_modules = [core]
 
 setup(
     name="libjulia",
     description="Python binding library for libjulia",
     author="Kenta Sato",
     packages=["libjulia"],
-    include_dirs=[np.get_include()],
-    ext_modules=cythonize(core)
+    ext_modules=ext_modules
 )
